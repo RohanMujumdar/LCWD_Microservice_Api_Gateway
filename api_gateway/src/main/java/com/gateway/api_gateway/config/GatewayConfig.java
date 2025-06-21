8c4cb5cbf1b23c763fd68d1bab991b5a68e801f8
Rohan Mujumdar <rohanmujumdar2001@gmail.com>
2025-06-21 17:12:14 +0530

    Initial Commit for API Gateway.

---
+++ api_gateway/src/main/java/com/gateway/api_gateway/config/GatewayConfig.java
@@ -0,0 +1,31 @@
+package com.gateway.api_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    // We are creating the routes to different services via Java Beans configs over here
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder)
+    {
+        return builder.routes().
+                route(
+                        "foodService",
+                        route->route.path("/food/**")
+                                .filters(f -> f.rewritePath("/food/?(?<remaining>.*)", "/$\\{remaining}"))
+                                .uri("lb://FOODSERVICE")
+                ).
+                route(
+                        "restaurantService",
+                        route->route.path("/restaurant/**")
+                                .filters(f -> f.rewritePath("/restaurant/?(?<remaining>.*)", "/$\\{remaining}"))
+                                .uri("lb://RESTAURANTSERVICE")
+                ).
+                build();
+
+    }
+}
