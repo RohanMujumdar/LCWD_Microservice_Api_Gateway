75ab26d998d6547c93f6083cb7987011befaac26
Rohan Mujumdar <rohanmujumdar2001@gmail.com>
2025-07-06 18:25:41 +0530

    Implemented circuit breaker and retry mechanism.

--- api_gateway/src/main/java/com/gateway/api_gateway/config/GatewayConfig.java
+++ api_gateway/src/main/java/com/gateway/api_gateway/config/GatewayConfig.java
@@ -1,9 +1,16 @@
 package com.gateway.api_gateway.config;
 
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @Configuration
 public class GatewayConfig {
@@ -16,13 +23,42 @@ public class GatewayConfig {
                 route(
                         "foodService",
                         route->route.path("/food/**")
-                                .filters(f -> f.rewritePath("/food/?(?<remaining>.*)", "/$\\{remaining}"))
+                                .filters(f -> f.rewritePath("/food/?(?<remaining>.*)", "/$\\{remaining}")
+                                        .circuitBreaker(circuitBreakerConfig -> circuitBreakerConfig.setName("circuitBreakerFood")
+                                                .setFallbackUri("forward:/circuitBreaker/fallback")
+                                        )
+                                        .requestRateLimiter(rateConfig->{
+//                                            Fill the implementation of rate limiter.
+                                            rateConfig.setRateLimiter(rateLimiter())
+//                                            Fill the implementation of key resolver.
+                                                    .setKeyResolver(keyResolver());
+                                        })
+
+                                )
                                 .uri("lb://FOODSERVICE")
                 ).
                 route(
                         "restaurantService",
                         route->route.path("/restaurant/**")
-                                .filters(f -> f.rewritePath("/restaurant/?(?<remaining>.*)", "/$\\{remaining}"))
+                                .filters(f -> f.rewritePath("/restaurant/?(?<remaining>.*)", "/$\\{remaining}")
+                                        .retry(retryConfig -> {
+                                            retryConfig.setRetries(3)
+                                                    .setMethods(HttpMethod.GET)
+                                                    .setBackoff(
+
+//                                                  After this much time, 1st retry will take place.
+                                                            Duration.ofMillis(100),
+
+//                                                  Final retry will go upto this much time
+                                                            Duration.ofMillis(800),
+
+//                                                  Duration of every retry will increase by the below factor
+                                                            2,
+
+//                                                  The duration will increase based on the previous value.
+                                                            true
+                                                    );
+                                        }))
                                 .uri("lb://RESTAURANTSERVICE")
                 ).
                 route(
@@ -35,4 +71,27 @@ public class GatewayConfig {
                 build();
 
     }
+
+
+
+    @Bean
+    public KeyResolver keyResolver()
+    {
+        return exchange -> {
+            return Mono.just(
+                    exchange.getRequest()
+                            .getRemoteAddress()
+                            .getAddress()
+                            .getHostAddress()
+            );
+        };
+    }
+
+    @Bean
+    public RedisRateLimiter rateLimiter()
+    {
+        return new RedisRateLimiter(1, 60, 60);
+    }
+
 }
+
---
+++ api_gateway/src/main/java/com/gateway/api_gateway/controller/FallBackController.java
@@ -0,0 +1,21 @@
+package com.gateway.api_gateway.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallBackController {
+
+
+    // This is the fallback mechanism
+    @RequestMapping("/circuitBreaker/fallback")
+    public Mono<ResponseEntity<String>> circuitBreakerFoodFallback()
+    {
+        return Mono.just(ResponseEntity
+                .status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("Food Service is down. Contact the service provider."));
+    }
+}
