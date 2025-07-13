3e6bbde6d24429319761159cee920d9c1a548f06
Rohan Mujumdar <rohanmujumdar2001@gmail.com>
2025-07-13 18:32:19 +0530

    Added a security mechanism to Api Gateway.

--- api_gateway/src/main/java/com/gateway/api_gateway/config/SecurityConfig.java
+++ api_gateway/src/main/java/com/gateway/api_gateway/config/SecurityConfig.java
@@ -1,18 +1,28 @@
 package com.gateway.api_gateway.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private RoleConverter roleConverter;
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity)
@@ -21,7 +31,8 @@ public class SecurityConfig {
         httpSecurity.cors(e->e.disable())
                 .csrf(e->e.disable())
                 .authorizeExchange(exchange->{
-                    exchange.pathMatchers(HttpMethod.GET, "/food/**").hasRole("ADMIN")
+                    exchange.pathMatchers(HttpMethod.GET).permitAll()
+                            .pathMatchers("/food/**").hasRole("ADMIN")
                             .pathMatchers(HttpMethod.POST, "/restaurant/**").hasRole("ADMIN")
                             .anyExchange().authenticated();
                 })
@@ -32,10 +43,27 @@ public class SecurityConfig {
 //               JWT handling (like decoding tokens using a configured JwtDecoder).
 
 //               In short: Enable JWT-based authentication with default settings.
-                .oauth2ResourceServer(config->
-                        config.jwt(Customizer.withDefaults()))
+//                .oauth2ResourceServer(config->
+//                        config.jwt(Customizer.withDefaults()))
 
-        ;
+                .oauth2ResourceServer(config->
+                        config.jwt(jwt-> jwt.jwtAuthenticationConverter(roleExtractor()))
+                );
+
+
         return httpSecurity.build();
+    }
+
+
+    public Converter<Jwt, Mono<AbstractAuthenticationToken>> roleExtractor()
+    {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+
+        converter.setJwtGrantedAuthoritiesConverter(roleConverter);
+
+
+//      Converts a normal JwtAuthenticationConverter object to a reactive one.
+//      This is because our project is reactive.
+        return new ReactiveJwtAuthenticationConverterAdapter(converter);
     }
 }
